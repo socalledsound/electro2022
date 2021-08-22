a597ee5adfcc5fdf1e06f3d0b7efe50a28cf01a0
Chris Kubick <socalledsound@gmail.com>
2021-08-22 10:59:03 -0700

    add fb config

---
+++ src/firebase/FIREBASE_CONFIG.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8P6hjXhYC1RRdlGVxoh_ya_ohzouThMc",
+    authDomain: "electro2021-84fab.firebaseapp.com",
+    projectId: "electro2021-84fab",
+    storageBucket: "electro2021-84fab.appspot.com",
+    messagingSenderId: "179307085458",
+    appId: "1:179307085458:web:a7f72185cacb171a1f704b",
+    measurementId: "G-QGL84E1S6V"
+  };
+
+  export default firebaseConfig
