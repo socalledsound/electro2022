3c776ce3e222a99501cd6320fa1600c098ace8df
Chris Kubick <socalledsound@gmail.com>
2021-08-28 14:27:01 -0700

    trying to flush gallery

--- src/app/store.js
+++ src/app/store.js
@@ -39,7 +39,7 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  blacklist:['syllabus','warnings', 'assignment', 'submitWork']
+  blacklist:['syllabus','warnings', 'assignment', 'submitWork', 'gallery']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
