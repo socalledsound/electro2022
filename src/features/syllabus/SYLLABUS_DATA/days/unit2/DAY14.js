bcc1115b513520972ec87bf39fe2536615ae1d39
Chris Kubick <socalledsound@gmail.com>
2021-09-23 12:58:50 -0700

    updates

--- src/features/syllabus/SYLLABUS_DATA/days/unit2/DAY14.js
+++ src/features/syllabus/SYLLABUS_DATA/days/unit2/DAY14.js
@@ -3,9 +3,10 @@ const DAY14 = {
     unit: 'unit2',
     date: 'October 12, 2021',
     title: 'control',
-    illustrationURL: 'https://res.cloudinary.com/chris-kubick/image/upload/v1596822596/side-effects/handswtf_p5ubjn.jpg',
+    illustrationURL: 'https://res.cloudinary.com/chris-kubick/image/upload/v1632426987/nes_t7dxfv.jpg',
     description: `
-    
+        We're back in the flesh today!  We'll look at how to wire up our CPE to our games so that we can
+        have novel forms of input, and generally troubleshoot our projects so that we're ready for Thursday's crit!
     `,
     discussionQuestions: [
         ``,
