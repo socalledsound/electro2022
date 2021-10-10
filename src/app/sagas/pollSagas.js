b7c958a67d5c7853864a7d68e8550ca411c61e9e
socalledsound <lrs6000@gmail.com>
2021-10-10 11:36:00 -0700

    updating sagas

--- src/app/sagas/pollSagas.js
+++ src/app/sagas/pollSagas.js
@@ -1,13 +1,26 @@
 import  { takeLatest, put, all, call, take } from 'redux-saga/effects';
 import { eventChannel } from 'redux-saga'
+import { startFetchPollAnswers, startSubmitPollAnswer } from '../../features/poll/pollSlice'
 
+function* submitPollAnswer(){
+    //need to figure out the firebase side of this
+}
 
-function* onSubmitPollAnswer(){
-    
+function* fetchPollAnswers(){
+
+}
+
+function* onStartSubmitPollAnswer(){
+    yield takeLatest(startSubmitPollAnswer.type, submitPollAnswer)
+}
+
+function* onStartFetchPollAnswers(){
+    yield takeLatest(startFetchPollAnswers.type, fetchPollAnswers)
 }
 
 export function* pollSagas(){
     yield all(call([
-        yield onSubmitPollAnswer()
+        yield onStartSubmitPollAnswer(),
+        yield onStartFetchPollAnswers(),
     ]))
 }
