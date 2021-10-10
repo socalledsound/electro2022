34395cb3f183f46797edfd3f220b5de88b43d757
socalledsound <lrs6000@gmail.com>
2021-10-10 11:37:14 -0700

    pollslice

--- src/features/poll/pollSlice.js
+++ src/features/poll/pollSlice.js
@@ -1,17 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
+import POLLQUESTIONS from './PollQuestions'
 
 const initialState = {
-
+    polls: POLLQUESTIONS,
+    pollAnswers: [],
 }
 
 export const pollSlice = createSlice({
     name: 'poll',
     initialState,
     reducers: {
-
+        startSubmitPollAnswer(){},
+        startFetchPollAnswers(){},
     }
 })
 
-
+export const {
+    startSubmitPollAnswer,
+    startFetchPollAnswers,
+} = pollSlice.actions
 
 export default pollSlice.reducer
