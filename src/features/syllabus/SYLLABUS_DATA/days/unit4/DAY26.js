ef3d0bbc37468b96b070f02d304cdf94b040272d
Chris Kubick <socalledsound@gmail.com>
2021-11-22 21:48:21 -0800

    update shader day syll

--- src/features/syllabus/SYLLABUS_DATA/days/unit4/DAY26.js
+++ src/features/syllabus/SYLLABUS_DATA/days/unit4/DAY26.js
@@ -7,17 +7,16 @@ const DAY26 = {
     assignment: 'ripples',
     illustrationURL: 'https://res.cloudinary.com/chris-kubick/image/upload/v1604102337/side-effects/light-shade-ripples-pattern-swimming-pool-turquoise-clean-water-vivid-separatio-lines-separation-113595482_agwgav.jpg',
     description: `
-    We'll continue our exploration of shaders today and use them with sound to make nifty visualizations that re-arrange 
+    Today we'll begin by looking at so-called Perlin noise, which is a nice way to generate gradient noise patterns.
+
+    Then, we'll We'll continue our exploration of shaders, and use them with sound to make nifty visualizations that re-arrange 
     the pixels of an image into ripples.
 
-    Finally, we'll pull back the curtain
-    just a bit and look at what we can do with shaders, by adding a generative wood pattern to our model.   
+    And, we'll take a little look at what we can do with shaders, by adding a generative wood pattern to our model.   
     Shaders are a bit daunting, written in a variant of C called shader language, as they are.  
     But, tapping into the power of shaders lets us render images a LOT faster, with more resolution, detail and speed.  
-    
-    WE'll learn how to add images as textures and also how to generate shaders using code --  
-    we'll use a simplex noise algorithm (aka perlin noise...) to make a wood grain pattern.
 
+    And, I'll help anyone who needs help with their projects!
     `,
     videos: [
         {
@@ -38,6 +37,7 @@ const DAY26 = {
             linkText: 'perlin noise mountains landscape',
             linkSrc: 'https://github.com/socalledsound/perlin-mountains',
         }, 
+        
         {
             linkText: 'sound circle waves on a sphere',
             linkSrc: 'https://editor.p5js.org/socalledsound/sketches/Lv1ebN2QBg',
@@ -97,6 +97,14 @@ const DAY26 = {
         
     ],
     techLinks: [
+        {
+            linkText: 'https://genekogan.com/code/p5js-perlin-noise/',
+            linkSrc: 'an intro to perlin noise',
+        },
+        {
+            linkText: 'learn perlin noise in p5 by example',
+            linkSrc: 'https://gorillasun.de/blog/Introduction-to-Perlin-Noise-in-P5JS-and-Processing',
+        },
         {
             linkText: 'mandelbrot shader',
             linkSrc: 'https://editor.p5js.org/socalledsound/sketches/8R_8wlC5G',
