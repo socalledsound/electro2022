1ddda3012349ff3bef8b034b9314fa2de0c7098e
ck <ck@cks-macbook-pro.lan>
2022-09-06 18:00:25 -0700

    tweak loading animation

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -9,11 +9,13 @@ const Loading = () => {
                     <div className={styles.circle1}></div>
                     <div className={styles.circle2}></div>
                     <div className={styles.circle3}></div>
+                    <div className={styles.circle4}></div>
+                    <div className={styles.circle5}></div>
                 </div>
 
             
             <div>
-                <p>...loading</p>
+                <p class={styles.loadingText}>...loading</p>
             </div>
         </div>
      );
